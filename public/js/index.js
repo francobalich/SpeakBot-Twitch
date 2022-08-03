c59e631b65d4f0276e959d6974169b331388ca8f
Franco Balich <franco.balich@gmail.com>
2022-08-03 15:13:48 -0300

    Fix format with eslint

--- public/js/index.js
+++ public/js/index.js
@@ -1,46 +1,46 @@
-const txtUsername = document.getElementById("txtUsername");
-const txtText = document.getElementById("txtText");
-const mensajes = document.getElementById("mensajes");
-const msgBox= document.getElementById("msgBox");
-//La comunicacion por Sockets la realizaremos por el puerto 3000
-const socket = io("http://localhost:3000/");
+const txtUsername = document.getElementById('txtUsername')
+const txtText = document.getElementById('txtText')
+const mensajes = document.getElementById('mensajes')
+const msgBox = document.getElementById('msgBox')
+// La comunicacion por Sockets la realizaremos por el puerto 3000
+const socket = io('http://localhost:3000/')
 
-let u = new SpeechSynthesisUtterance();
-u.text = '';
-u.lang = 'es-ES';
-u.rate = 1; 
+const u = new SpeechSynthesisUtterance()
+u.text = ''
+u.lang = 'es-ES'
+u.rate = 1
 
-//Funciones
-const enviarMensaje=(etiqueta,mensaje)=>{
-    socket.emit(etiqueta,mensaje);
+// Funciones
+const enviarMensaje = (etiqueta, mensaje) => {
+  socket.emit(etiqueta, mensaje)
 }
-const ocultarMensaje=()=>{
-    console.log("Fin timer")
-    msgBox.style.width="0px";
+const ocultarMensaje = () => {
+  console.log('Fin timer')
+  msgBox.style.width = '0px'
 }
 
-//Socket para el nombre de usaurio
-socket.on('username', (data)=>{
-    txtUsername.innerHTML = data;
-});
-//Socket para el texto del mensaje
-socket.on('text', (data)=>{
-    msgBox.style.width="600px";
-    txtText.innerHTML = data;
-    setTimeout(ocultarMensaje, 10000);
-});
-//Sockets para recibir el html del emote
-socket.on('emote', (data)=>{
-    emoteContainer.innerHTML =data;
-}); 
+// Socket para el nombre de usaurio
+socket.on('username', (data) => {
+  txtUsername.innerHTML = data
+})
+// Socket para el texto del mensaje
+socket.on('text', (data) => {
+  msgBox.style.width = '600px'
+  txtText.innerHTML = data
+  setTimeout(ocultarMensaje, 10000)
+})
+// Sockets para recibir el html del emote
+socket.on('emote', (data) => {
+  emoteContainer.innerHTML = data
+})
 
-//Sockets para el mensaje a hablar
-socket.on('speak', (data)=>{
-    u.text = data;
-    speechSynthesis.speak(u);
-});
-//Sockets para la respuesta a la pregunta que le hicimos al bot
-socket.on('pregunta', (data)=>{
-    u.text = data;
-    speechSynthesis.speak(u);
-});
+// Sockets para el mensaje a hablar
+socket.on('speak', (data) => {
+  u.text = data
+  speechSynthesis.speak(u)
+})
+// Sockets para la respuesta a la pregunta que le hicimos al bot
+socket.on('pregunta', (data) => {
+  u.text = data
+  speechSynthesis.speak(u)
+})
--- src/index.js
+++ src/index.js
@@ -1,177 +1,173 @@
 // Require necesario para leer las variables de entorno del archivo .env
-require("dotenv").config();
+require('dotenv').config()
 // Instanciación de los objetos necesarios para crear un servidor local con Node.JS
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT;
-const app = express();
-const chatDialogs = require("./json/chats")
-const chistes = require("./json/chistes")
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT
+const app = express()
+const chatDialogs = require('./json/chats')
+const chistes = require('./json/chistes')
 // Paquete para poder crear el bot de twitch que puede recibir los mensajes del chat
-const tmi = require("tmi.js");
+const tmi = require('tmi.js')
 
 // Instanciación de los objetos necesarios para la comunicación por Sockets
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 
-//Vamos a utilizar Axios para acceder a la API de Twitch y poder obtener los link de los Emotes
-const axios = require('axios').default;
+// Vamos a utilizar Axios para acceder a la API de Twitch y poder obtener los link de los Emotes
+const axios = require('axios').default
 
 // Datos de la cuenta de Twitch que utilizaran como bot que se leen el .env
 // Documentación https://dev.twitch.tv/docs/irc
-const options= {
-    options:{
-        debug:true
-    },
-    connection:{
-        
-    },
-    identity: {
-        username: process.env.USERNAME,//Nombre de usuario de la cuenta que se utilizara como bot
-        password: process.env.PASSWORD //Se utiliza el oauth como contraseña
-        // Para generar el oauth: https://twitchapps.com/tmi/
-      },
-      channels: [
-        process.env.CHANNEL //Nombre de usuario de la cuenta en la que el bot leera el chat
-      ]
-};
+const options = {
+  options: {
+    debug: true
+  },
+  connection: {
+
+  },
+  identity: {
+    username: process.env.USERNAME, // Nombre de usuario de la cuenta que se utilizara como bot
+    password: process.env.PASSWORD // Se utiliza el oauth como contraseña
+    // Para generar el oauth: https://twitchapps.com/tmi/
+  },
+  channels: [
+    process.env.CHANNEL // Nombre de usuario de la cuenta en la que el bot leera el chat
+  ]
+}
 
 // Importa el archivo JSON con los chats para el bot
 
 // Inicio server Node.JS
-const iniciarServer=()=>{
-  server.listen(port,()=>{
-    console.log("El programa se esta ejecutando en el puerto: "+ port)
-  });
+const iniciarServer = () => {
+  server.listen(port, () => {
+    console.log('El programa se esta ejecutando en el puerto: ' + port)
+  })
   // Obtiene la ruta del directorio publico donde se encuentran los elementos estaticos (css, js).
-  var publicPath = path.resolve(__dirname, '../public'); 
+  const publicPath = path.resolve(__dirname, '../public')
   // Para que los archivos estaticos queden disponibles.
-  app.use(express.static(publicPath));
-  
-  app.get('/', function(req, res){
-    res.sendFile(__dirname + '../public/index.html');
-  });
+  app.use(express.static(publicPath))
+
+  app.get('/', function (req, res) {
+    res.sendFile(__dirname + '../public/index.html')
+  })
 }
 // Instanciamos el cliente de tmi (bot)
-const client = new tmi.client(options);
+const client = new tmi.client(options)
 
 // SOCKETS
 // Funcion que envia por sockets los datos del ultimo mensaje del chat
-const refreshFront=(username,msg)=>{
-  io.emit("username",username);
-  io.emit("text",msg);
+const refreshFront = (username, msg) => {
+  io.emit('username', username)
+  io.emit('text', msg)
 }
-// Función que envia por sockets el mensaje a leer 
-const hablar=(username,msg)=>{
-  let mensaje;
-  if(msg==''){
-    mensaje =`Escribe algo,${username}`;
+// Función que envia por sockets el mensaje a leer
+const hablar = (username, msg) => {
+  let mensaje
+  if (msg === '') {
+    mensaje = `Escribe algo,${username}`
+  } else {
+    mensaje = `${username} dice ${msg}`
   }
-  else{
-    mensaje =`${username} dice ${msg}`;
-  }
-  io.emit("speak",`${mensaje}`);
+  io.emit('speak', `${mensaje}`)
 }
-// Función que envia por sockets el mensaje a leer 
-const leer=(msg)=>{
-  if(msg!==''){
-    io.emit("speak",`${msg}`);
+// Función que envia por sockets el mensaje a leer
+const leer = (msg) => {
+  if (msg !== '') {
+    io.emit('speak', `${msg}`)
   }
-  
 }
 // Edita el mensaje del chat, para insertar los html necesario para agregar las imagenes de los emotes en los mensajes
-const msgEdit= async(ctx,msg)=>{
-  if (ctx.emotes!=null){
-    var msgEditado=msg;
+const msgEdit = async (ctx, msg) => {
+  if (ctx.emotes != null) {
+    let msgEditado = msg
     for (const emote in ctx.emotes) {
-      const linkEmote = (await getEmote(emote)).config.url;
-      const etiquetaEmote =`<img src="${linkEmote}" alt="emote">`
-      
+      const linkEmote = (await getEmote(emote)).config.url
+      const etiquetaEmote = `<img src="${linkEmote}" alt="emote">`
+
       for (const pos in ctx.emotes[emote]) {
-        let inicio=0;
-        let final=0;
-        let posicion =ctx.emotes[emote][pos];
-        inicio=posicion.split('-')[0];
-        final=posicion.split('-')[1];
-        let nameEmote= msg.substring(inicio,parseInt(final)+1);
-        msgEditado=msgEditado.replace(`${nameEmote}`,etiquetaEmote);
+        let inicio = 0
+        let final = 0
+        const posicion = ctx.emotes[emote][pos]
+        inicio = posicion.split('-')[0]
+        final = posicion.split('-')[1]
+        const nameEmote = msg.substring(inicio, parseInt(final) + 1)
+        msgEditado = msgEditado.replace(`${nameEmote}`, etiquetaEmote)
       }
     }
-    return msgEditado;
-  }
-  else{
-    return msg;
+    return msgEditado
+  } else {
+    return msg
   }
 }
 // Función que accede a la API de Twitch
-const getEmote = async (id) =>{
-  const apiURL = `https://static-cdn.jtvnw.net/emoticons/v2/${id}/default/light/1.0`;
-  const res= await axios.get(apiURL);
-  return res;
+const getEmote = async (id) => {
+  const apiURL = `https://static-cdn.jtvnw.net/emoticons/v2/${id}/default/light/1.0`
+  const res = await axios.get(apiURL)
+  return res
 }
 
 // Función que simula la tirada de una dato
 function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
+  const sides = 6
+  return Math.floor(Math.random() * sides) + 1
 }
 // Evento que se ejecuta cuando el bot se conecta al chat de Twitch
-client.on('connected', ()=>{
+client.on('connected', () => {
   // Agregue el código que quiera que se ejecute en esta situación
-});
+})
 // Evento que se ejecuta cuando se envia un mensaje en el chat
-client.on("chat",async(target,ctx,message,seft)=>{
-    if(seft)return;
-    let mensaje= message.split(' ');
-    const commandName = message.trim()
-    if (commandName.substring(0, 1) === "!") {
-
-    let msgRespuesta = chatDialogs[commandName];
-    if (msgRespuesta != undefined) {
-      if (msgRespuesta.includes("&USER&")) {
-        let msgOriginal = msgRespuesta.split("&");
-        msgRespuesta = msgOriginal[0] + ctx.username + msgOriginal[2];
+client.on('chat', async (target, ctx, message, seft) => {
+  if (seft) return
+  const mensaje = message.split(' ')
+  const commandName = message.trim()
+  if (commandName.substring(0, 1) === '!') {
+    let msgRespuesta = chatDialogs[commandName]
+    if (msgRespuesta !== undefined) {
+      if (msgRespuesta.includes('&USER&')) {
+        const msgOriginal = msgRespuesta.split('&')
+        msgRespuesta = msgOriginal[0] + ctx.username + msgOriginal[2]
       }
-      client.say(target, msgRespuesta);
+      client.say(target, msgRespuesta)
     }
-    if(commandName==="!dado"){
-      let num = rollDice();
-      client.say(target,`Sacaste un ${num}`);
+    if (commandName === '!dado') {
+      const num = rollDice()
+      client.say(target, `Sacaste un ${num}`)
     }
-    if(mensaje[0]==="!speak"){
-      let textoMensaje=message.replace(mensaje[0],'');
-      hablar(ctx.username,textoMensaje);
-    }if(mensaje[0]==="!chiste"){
-      let longitud =Object.keys(chistes).length
-      let numChiste = Math.floor(Math.random() * longitud);
-          let chiste = chistes[numChiste];
-          console.log(chiste);
-          leer(chiste);
+    if (mensaje[0] === '!speak') {
+      const textoMensaje = message.replace(mensaje[0], '')
+      hablar(ctx.username, textoMensaje)
+    } if (mensaje[0] === '!chiste') {
+      const longitud = Object.keys(chistes).length
+      const numChiste = Math.floor(Math.random() * longitud)
+      const chiste = chistes[numChiste]
+      console.log(chiste)
+      leer(chiste)
     }
-    if(ctx.username!="streamelements"){
-      const mensajeTratado= await msgEdit(ctx,message);
-      refreshFront(ctx.username,mensajeTratado);
-    } }
-    
-    if (commandName === "!help" || commandName === "!command") {
-      let comandos="";
-      
-      Object.keys(chatDialogs).forEach(comando => {
-        comandos += comando +" - ";
-      });
-      let ultimo= comandos.lastIndexOf(" - ");
-      let comandosFijos = [" - !speak - !dado - !chiste - !help"]
-      comandos=comandos.substring(0,ultimo);
-      client.say(
-        target,
-        `Los comandos disponibles son: `+comandos + comandosFijos
-      );
-      console.log(target)
+    if (ctx.username != 'streamelements') {
+      const mensajeTratado = await msgEdit(ctx, message)
+      refreshFront(ctx.username, mensajeTratado)
     }
+  }
+
+  if (commandName === '!help' || commandName === '!command') {
+    let comandos = ''
+
+    Object.keys(chatDialogs).forEach(comando => {
+      comandos += comando + ' - '
+    })
+    const ultimo = comandos.lastIndexOf(' - ')
+    const comandosFijos = [' - !speak - !dado - !chiste - !help']
+    comandos = comandos.substring(0, ultimo)
+    client.say(
+      target,
+      'Los comandos disponibles son: ' + comandos + comandosFijos
+    )
+    console.log(target)
+  }
 })
 
 // Inicia el server Node.JS
-iniciarServer();
+iniciarServer()
 // Conecta el cliente tmi (bot)
-client.connect();
-
+client.connect()
