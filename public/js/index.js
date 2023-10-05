7b68eda778cde57382835ffce0ed12ca43ba2d92
Franco Balich <franco.balich@gmail.com>
2023-10-05 20:55:16 -0300

    Change: Delete old tts

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,12 @@
-/* global io, SpeechSynthesisUtterance, speechSynthesis, tts */
 /* eslint no-undef: ["error", { "typeof": true }] */
+/* global io, tts */
 
 const txtUsername = document.getElementById('txtUsername')
 const txtText = document.getElementById('txtText')
 const msgBox = document.getElementById('msgBox')
+
 // La comunicacion por Sockets la realizaremos por el puerto 3000
 const socket = io('http://localhost:5100/')
-
-// Crear multiples instancias una para cada mensaje
-const u = new SpeechSynthesisUtterance()
-u.text = ''
-u.lang = 'es-ES'
-u.rate = 1
 
 // Funciones
 const ocultarMensaje = () => {
@@ -33,14 +28,11 @@ socket.on('text', (data) => {
 // Sockets para el mensaje a hablar
 socket.on('speak', async (data) => {
   const resp = await JSON.parse(data)
-  u.text = resp.message
-  // speechSynthesis.speak(u)
   txtUsername.style.color = resp.color
   ttsSpeak(resp.message)
 })
 
 // Sockets para la respuesta a la pregunta que le hicimos al bot
 socket.on('pregunta', (data) => {
-  u.text = data
-  speechSynthesis.speak(u)
+  ttsSpeak(data)
 })
