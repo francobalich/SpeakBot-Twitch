a18593e7a965e1c101d1c3f6b7b440ce763f53c3
Franco Balich <franco.balich@gmail.com>
2023-10-05 20:52:30 -0300

    Feat: Add new tts

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,13 @@
-/* global io, SpeechSynthesisUtterance, speechSynthesis */
+/* global io, SpeechSynthesisUtterance, speechSynthesis, tts */
 /* eslint no-undef: ["error", { "typeof": true }] */
 
 const txtUsername = document.getElementById('txtUsername')
 const txtText = document.getElementById('txtText')
 const msgBox = document.getElementById('msgBox')
 // La comunicacion por Sockets la realizaremos por el puerto 3000
-const socket = io('http://localhost:3000/')
+const socket = io('http://localhost:5100/')
 
+// Crear multiples instancias una para cada mensaje
 const u = new SpeechSynthesisUtterance()
 u.text = ''
 u.lang = 'es-ES'
@@ -16,7 +17,9 @@ u.rate = 1
 const ocultarMensaje = () => {
   msgBox.style.right = '-1000px'
 }
-
+const ttsSpeak = (message) => {
+  tts.speak(message, 'es-AR', 1)
+}
 // Socket para el nombre de usaurio
 socket.on('username', (data) => {
   txtUsername.innerHTML = data
@@ -31,9 +34,11 @@ socket.on('text', (data) => {
 socket.on('speak', async (data) => {
   const resp = await JSON.parse(data)
   u.text = resp.message
-  speechSynthesis.speak(u)
+  // speechSynthesis.speak(u)
   txtUsername.style.color = resp.color
+  ttsSpeak(resp.message)
 })
+
 // Sockets para la respuesta a la pregunta que le hicimos al bot
 socket.on('pregunta', (data) => {
   u.text = data
---
+++ public/js/tts.js
@@ -0,0 +1,53 @@
+/* global SpeechSynthesisUtterance, speechSynthesis */
+
+class TTS {
+  #voices
+  #onReady
+  #ready
+  #whenSinthReady
+  constructor () {
+    this.#voices = {}
+    this.#onReady = []
+    this.#ready = false
+    this.#whenSinthReady = () => {
+      speechSynthesis.getVoices().forEach((voice) => {
+        const { lang } = voice
+        if (lang.length === 5) {
+          (this.#voices[lang]
+            ? (this.#voices[lang] += 1)
+            : (this.#voices[lang] = 1))
+        }
+        this.#ready = true
+        this.#onReady.forEach((cb) => { cb(this.#voices) })
+        speechSynthesis.removeEventListener('voiceschanged', this.#whenSinthReady)
+      })
+    }
+    speechSynthesis.addEventListener('voiceschanged', this.#whenSinthReady)
+  }
+
+  speak (msj, accent, variant) {
+    const toSpeak = new SpeechSynthesisUtterance(msj)
+    toSpeak.voice = this.#voices[accent]
+      ? variant <= this.#voices[accent]
+        ? speechSynthesis.getVoices().filter(
+          (voice) => voice.lang === accent
+        )[variant - 1]
+        : speechSynthesis.getVoices().filter(
+          (voice) => voice.lang === accent
+        )[0]
+      : speechSynthesis.getVoices()[0]
+    console.log(variant)
+    console.log(toSpeak)
+    speechSynthesis.speak(toSpeak)
+  }
+
+  getVoices () {
+    return structuredClone(this.#voices)
+  }
+
+  whenReady (cb) {
+    this.#ready ? cb(this.#voices) : this.#onReady.push(cb)
+  }
+}
+
+const tts = new TTS()
